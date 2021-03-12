cfd676dfdbdd479e4866d2c2e48b358789b74081
ivulovic <ivn.vulovic@gmail.com>
2021-03-12 20:55:29 +0100

    Updates

--- client/legacy/index.7980a491.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./client.0dbd80e8.js";function o(o){var i=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{},e=i.delay,n=void 0===e?0:e,a=i.duration,d=void 0===a?400:a,r=i.easing,c=void 0===r?t:r,s=+getComputedStyle(o).opacity;return{delay:n,duration:d,easing:c,css:function(t){return"opacity: ".concat(t*s)}}}export{o as f};
