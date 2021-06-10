481cf2a440b13503107c76736be1bee1da5b53ef
ivulovic <ivn.vulovic@gmail.com>
2021-06-10 23:48:57 +0200

    Updates

---
+++ client/index.a28fdc80.js
@@ -0,0 +1,1 @@
+import{I as t}from"./client.ab6114d9.js";function a(a,{delay:e=0,duration:o=400,easing:i=t}={}){const n=+getComputedStyle(a).opacity;return{delay:e,duration:o,easing:i,css:t=>"opacity: "+t*n}}export{a as f};
